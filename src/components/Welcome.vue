8b2a3e892b5e72d558744402992cbe778ac4e86f
Ashton Basile <ashton.basile@gmail.com>
2021-08-12 19:36:16 +0200

    the first page is transferred over.

---
+++ src/components/Welcome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to the Flash Card App!</h1>
+    <p>You currently have 0 decks in your library. Please enter the name of your new deck into the textbox:</p>
+    <img src="../assets/flash_cards.png" alt="Flash Cards">
+    <br>
+    <a href="deck_view.html" target="_self">Navigate to the Deck View Page</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
