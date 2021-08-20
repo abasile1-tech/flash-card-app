f6df1733137d4c76dc8e46113ebf6c0b472f166f
Ashton Basile <ashton.basile@gmail.com>
2021-08-20 19:54:16 +0200

    formatted the card

--- src/views/SingleDeck.vue
+++ src/views/SingleDeck.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <h1>{{deckName}}</h1>
-        <div>
+        <div class="card">
             <p >{{cardSide}}</p>
             <p>{{cardPrompt}}</p>
-            <button>Flip Card</button>
+            <button id="flip">Flip Card</button>
         </div>
     </div>
 </template>
@@ -27,5 +27,20 @@ export default {
 </script>
 
 <style scoped>
-
+#flip {
+    width: 8em;
+    margin: 0em auto;
+}
+.card {
+    border: 0.1em solid rgb(131, 131, 131);
+    border-radius: 0.5em;
+    padding: 0em 0em 1.5em 0em;
+    width: 70%;
+    max-width:20em;
+    height: 20em;;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
 </style>
