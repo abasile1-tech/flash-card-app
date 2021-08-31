081efb570a5f7f96a92b14852b44e51e4086be52
Ashton Basile <ashton.basile@gmail.com>
2021-08-31 13:52:56 +0200

    finished the video Full stack vue.js, express and MongoDB 1 by traversy media

--- server/routes/api/decks.js
+++ server/routes/api/decks.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const mongodb = require('mongodb');
 
@@ -5,15 +6,28 @@ const router = express.Router();
 
 // Get Decks
 router.get('/', async (req, res) => {
-    const decks = await loadPostsCollection();
+    const decks = await loadDecksCollection();
     res.send(await decks.find({}).toArray());
 });
 
 // Add Deck
+router.post('/', async (req, res) => {
+    const decks = await loadDecksCollection(); 
+    await decks.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
 
 // Delete Deck
+router.delete('/:id', async (req, res) => {
+    const decks = await loadDecksCollection(); 
+    await decks.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    res.status(200).send();
+});
 
-async function loadPostsCollection() {
+async function loadDecksCollection() {
     const client = await mongodb.MongoClient.connect
     ('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
         useNewUrlParser: true
