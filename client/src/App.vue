50cb72d9f3c87a36cf96d12099bc67fde64b8ec5
Ashton Basile <ashton.basile@gmail.com>
2021-09-10 13:22:56 +0200

    changed the font

--- client/src/App.vue
+++ client/src/App.vue
@@ -25,11 +25,16 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  /* font-family: 'Roboto', sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+#app, button, input {
+  font-family: 'Roboto', sans-serif;
+}
+
 </style>
